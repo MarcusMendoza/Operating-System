9f9c04e86ff901390732dc05157996a67d31ace5
MarcusMendoza <7mmendoza@stpatrick.org>
2016-10-12 13:56:30 -0500

    work day 1 on operating system

---
+++ AppObject.js
@@ -0,0 +1,36 @@
+function App(n,m,s){
+
+    //Declare your instance variables here.
+    this.name= n
+    this.memory = m
+    this.state = s
+
+    //Declare the instance function open here.
+    this.open = function(){
+      this.state=applist[]
+
+
+
+    };
+    //Declare the instance function sleep here.
+
+
+
+    //Declare the instance function active here.
+
+
+
+    //Declare the instance function close here.
+
+
+
+
+}
+
+//Use the constructor to create 4 App objects below.
+
+
+
+//Declare an array named appList, and place all 4 of your apps in that array
+applist[]
+//The order does not matter.
